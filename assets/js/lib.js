d0dfec74b87ac47afe9ea9377566193599c12b2a
Ben Fowler <ben.fowler84@gmail.com>
2015-07-02 15:10:48 +0100

    Updated modal js

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -16182,7 +16182,7 @@ jQuery(document).ready(function($) {
 	//Initialize login and register links.
 	$('.login-link').click(function(){
 		$('.tab-content .tab-pane').removeClass('active');
-		$('.tab-content #login_tab').addClass('active');
+		$('.tab-content #login_tab').addClass('active fade in');
 		jQuery('#alimir_bootmodal').modal('show');
 	});
 	$('.add-more').click(function(){
@@ -16227,60 +16227,6 @@ jQuery(document).ready(function($) {
 			});
         e.preventDefault();
     });
-	// // Perform AJAX register on form submit
-	// $('form#regform').on('submit', function(e) {
-	// 		$.ajax({
-	// 			type: 'POST',
-	// 			dataType: 'json',
-	// 			url: ajax_login_object.ajaxurl,
-	// 			data: { 
-	// 				'action': 'ajaxregister', //calls wp_ajax_nopriv_ajaxlogin
-	// 				'user_login': $('form#regform #user_login').val(), 
-	// 				'first_name': $('form#regform #first_name').val(),
-	// 				'surname': $('form#regform #surname').val(),
-	// 				'publication': $('form#regform #publication').val(),
-	// 				'job_title': $('form#regform #job_title').val(),
-	// 				'user_pass': $('form#regform #user_pass').val(),
-	// 				'security2': $('form#regform #security2').val() },
-	// 			beforeSend:function(){
-	// 				$('#pass-submit').button('loading');
-	// 				$('body').modalmanager('loading');
-	// 			},						
-	// 			success: function(data){
-	// 				$('body').modalmanager('removeLoading');
-	// 				$('#pass-submit').button('reset');
-	// 				$('form#regform div.status').html(data.message).fadeIn();
-	// 				if (data.registered == true && ajax_login_object.registerRedirectURL!=''){
-	// 					window.location.href = ajax_login_object.registerRedirectURL;
-	// 				}					
-	// 			}
-	// 		});
-	// 	e.preventDefault();
-	// });
-	// Perform AJAX resetpass on form submit
-	$('form#passform').on('submit', function(e) {
-			$.ajax({
-				type: 'POST',
-				dataType: 'json',
-				url: ajax_login_object.ajaxurl,
-				data: {
-					'action'     : 	'ajaxlostpass', // Calls our wp_ajax_nopriv_ajaxlogin
-					'lost_pass'   : 	$('form#passform #lost_pass').val(),
-					'lostpass_captcha'   : 	$('form#passform #lostpass_captcha').val(),
-					'security3'   : 	$('form#passform #security3').val()
-				},
-				beforeSend:function(){
-					$('#user-submit').button('loading');
-					$('body').modalmanager('loading');
-				},								
-				success: function(data){
-					$('body').modalmanager('removeLoading');
-					$('#user-submit').button('reset');
-					$('form#passform div.status').html(data.message).fadeIn();
-				}
-			});
-			e.preventDefault();
-	});
 	// refresh login captcha
 	$("#login_captcha_img").click(function() {
 		document.getElementById('login_captcha_img').src=ajax_login_object.captchaLink + "log-captcha.php";
